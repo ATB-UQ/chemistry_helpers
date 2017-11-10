439202be52d53c0da0bfde2ffc521f5805559a11
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-11-10 15:28:44 +1000

    Feature: Add errors='strict' key word arguments to io encode/decode

--- io.py
+++ io.py
@@ -2,18 +2,18 @@ from typing import Union
 
 DEFAULT_ENCODING = 'utf8'
 
-def decode_if_necessary(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING) -> str:
-    return (db_object.decode(encoding) if isinstance(db_object, bytes) else db_object)
+def decode_if_necessary(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING, errors: str = 'strict') -> str:
+    return (db_object.decode(encoding, errors=errors) if isinstance(db_object, bytes) else db_object)
 
-def encode_if_necessary(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING) -> bytes:
-    return (db_object.encode(encoding) if isinstance(db_object, str) else db_object)
+def encode_if_necessary(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING, errors: str = 'strict') -> bytes:
+    return (db_object.encode(encoding, errors=errors) if isinstance(db_object, str) else db_object)
 
-def can_encode_and_decode(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING) -> bool:
+def can_encode_and_decode(db_object: Union[str, bytes], encoding: str = DEFAULT_ENCODING, errors: str = 'strict') -> bool:
     try:
         if isinstance(db_object, bytes):
-            return (encode_if_necessary(decode_if_necessary(db_object)) == db_object)
+            return (encode_if_necessary(decode_if_necessary(db_object, errors=errors), errors=errors) == db_object)
         elif isinstance(db_object, str):
-            return (decode_if_necessary(encode_if_necessary(db_object)) == db_object)
+            return (decode_if_necessary(encode_if_necessary(db_object, errors=errors), errors=errors) == db_object)
         else:
             raise Exception(type(db_object))
     except (UnicodeEncodeError, UnicodeDecodeError):
